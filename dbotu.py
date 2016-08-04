bf2851e316ced8d98ff49ccacade325ecfd55e21
swo <swo@mit.edu>
2016-08-04 12:38:42 -0400

    works with kmers

--- dbotu.py
+++ dbotu.py
@@ -4,20 +4,25 @@
 
 import argparse, sys
 import pandas as pd, numpy as np
+from Bio import SeqIO
 import scipy.stats
+
+from dbotu import kmer
 
 class DBCaller:
     '''
     Object for processing the sequence table and distance matrix into an OTU table.
     '''
-    def __init__(self, seq_table, matrix, max_dist, min_fold, threshold_pval, log=None, verbose=False):
+    def __init__(self, seq_table, records, k, max_dist, min_fold, threshold_pval, log=None, verbose=False):
         '''
         seq_table: pandas.DataFrame
           Samples on the columns; sequences on the rows
-        matrix: pandas.DataFrame
-          Symmetric, square matrix with sequences on the rows and columns
+        records: sequence of Bio.Seq
+          unaligned input sequences
+        k: int
+          k for the k-mers
         max_dist: float
-          Genetic cutoff above which a sequence will not be merged into an OTU
+          kmer distance cutoff above which a sequence will not be merged into an OTU
         min_fold: float
           Multiply the sequence's abundance by this fold to get the minimum abundance
           of an OTU for merging
@@ -35,7 +40,8 @@ class DBCaller:
           For every distribution test, a line "distribution_check"-sequence-OTU-pvalue.
         '''
         self.seq_table = seq_table
-        self.matrix = matrix
+        self.records = records
+        self.k = k
         self.max_dist = max_dist
         self.min_fold = min_fold
         self.threshold_pval = threshold_pval
@@ -45,32 +51,29 @@ class DBCaller:
         if self.verbose and self.log is None:
             raise RuntimeError("verbose option requires a log")
 
-        if not (self.matrix.transpose() == self.matrix).all().all():
-            raise RuntimeError("distance matrix must be symmetric")
-
-        if not (self.matrix.index.sort_values() == self.seq_table.index.sort_values()).all():
-            raise RuntimeError("distance matrix and sequence table must have same sequence IDs")
-
         # get a list of the names of the sequences in order of their (decreasing) abundance
         self.seq_abunds = self.seq_table.sum(axis=1).sort_values(ascending=False)
 
-        # get a list of the sequences to be used, which will be progressively popped
-        self.seqs = self.seq_abunds.index
-
-        # initialize an OTU table
+        # initialize the OTU information
         self.otu_table = pd.DataFrame(columns=self.seq_table.columns)
         self.otu_abunds = pd.Series(index=self.otu_table.columns)
+        self.otu_kmers = {}
 
-    def ga_candidates(self, seq):
+    def ga_candidates(self, record):
         '''OTUs that meet the genetic and abundance criteria'''
-        dists_to_otus = self.matrix[seq][self.otu_table.index]
+        #dists_to_otus = self.matrix[seq][self.otu_table.index]
+        seq_kmers = kmer.kmer_dict(str(record.seq), self.k)
+        dists_to_otus = pd.Series({otu: kmer.distance(seq_kmers, self.otu_kmers[otu]) for otu in self.otu_table.index})
         candidates1 = dists_to_otus[dists_to_otus < self.max_dist].sort_values(ascending=True).index
+
+        for c in candidates1:
+            print('genetic dist from', c, dists_to_otus[c], file=self.log)
 
         if self.verbose:
             print('genetic_check', *candidates1, sep='\t', file=self.log)
 
         candidate_abunds = self.otu_abunds[candidates1]
-        cutoff = self.seq_abunds[seq] * self.min_fold
+        cutoff = self.seq_abunds[record.id] * self.min_fold
         candidates2 = candidate_abunds[candidate_abunds > cutoff].index
 
         if self.verbose and len(candidates1) > 0:
@@ -78,20 +81,21 @@ class DBCaller:
 
         return candidates2
 
-    def merge_seq_into_otu(self, seq, otu):
+    def merge_seq_into_otu(self, seq_id, otu):
         '''Merge sequence into OTU, adjusting the OTU abundance'''
-        self.otu_table.loc[otu] += self.seq_table.loc[seq]
-        self.otu_abunds[otu] += self.seq_abunds[seq]
+        self.otu_table.loc[otu] += self.seq_table.loc[seq_id]
+        self.otu_abunds[otu] += self.seq_abunds[seq_id]
 
-    def create_otu(self, seq):
+    def create_otu(self, record):
         '''
         Create a new OTU by starting a new line in the OTU table,
         creating an OTU abundance entry, and ensuring the the OTU
         abundances remain sorted.
         '''
-        self.otu_table = self.otu_table.append(self.seq_table.loc[seq])
-        self.otu_abunds = self.otu_abunds.append(pd.Series(self.seq_abunds[seq], index=[seq]))
+        self.otu_table = self.otu_table.append(self.seq_table.loc[record.id])
+        self.otu_abunds = self.otu_abunds.append(pd.Series(self.seq_abunds[record.id], index=[record.id]))
         self.otu_abunds.sort_values(ascending=False, inplace=True)
+        self.otu_kmers[record.id] = kmer.kmer_dict(str(record.seq), self.k)
 
     @staticmethod
     def _D_helper(x):
@@ -119,33 +123,35 @@ class DBCaller:
         df = len(x) - 1
         return scipy.stats.chi2.sf(cls._D(x, y), df=df)
 
-    def _process_seq(self, seq):
+    def _process_record(self, record):
         '''
-        Process a sequence: run the genetic, abundance, and distribution checks, either
+        Process the next sequence: run the genetic, abundance, and distribution checks, either
         merging the sequence into an existing OTU or creating a new OTU.
         '''
-        if self.verbose:
-            print('seq', seq, sep='\t', file=self.log)
+        assert record.id in self.seq_table.index
 
-        for otu in self.ga_candidates(seq):
-            test_pval = self.abundance_test_pval(self.otu_table.loc[otu], self.seq_table.loc[seq])
+        if self.verbose:
+            print('seq', record.id, sep='\t', file=self.log)
+
+        for otu in self.ga_candidates(record):
+            test_pval = self.abundance_test_pval(self.otu_table.loc[otu], self.seq_table.loc[record.id])
 
             if self.verbose:
                 print('distribution_check', otu, test_pval, sep='\t', file=args.log)
 
             if test_pval > self.threshold_pval:
                 if self.log is not None:
-                    print('match', seq, otu, sep='\t', file=args.log)
+                    print('match', record.id, otu, sep='\t', file=args.log)
 
-                self.merge_seq_into_otu(seq, otu)
+                self.merge_seq_into_otu(record.id, otu)
                 return otu
 
         # form own otu
         if self.log is not None:
-            print('otu', seq, sep='\t', file=args.log)
+            print('otu', record.id, sep='\t', file=args.log)
 
-        self.create_otu(seq)
-        return seq
+        self.create_otu(record)
+        return record
 
     def generate_otu_table(self):
         '''
@@ -154,8 +160,8 @@ class DBCaller:
         returns: pandas.DataFrame
           OTU table (which can also be found at instance.otu_table)
         '''
-        for seq in self.seqs:
-            self._process_seq(seq)
+        for record in self.records:
+            self._process_record(record)
 
         return self.otu_table
 
@@ -171,26 +177,14 @@ def read_sequence_table(fn):
     '''
     return pd.read_table(fn, index_col=0, header=0).astype(int)
 
-def read_fasttree_matrix(fn):
-    '''
-    Read in a matrix produced by FastTree -makematrix
-    
-    fn: filename (or handle)
-
-    return: pandas.DataFrame
-    '''
-    # FastTree put it in a funny format: the number of rows in its own line at the top,
-    # then each row gets a label
-    matrix = pd.read_table(fn, sep='\s+', skiprows=1, header=None, index_col=0)
-    matrix.columns = matrix.index
-    return matrix
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser(description='', )
-    p.add_argument('matrix', type=argparse.FileType('r'), help='distance matrix from FastTree')
     p.add_argument('table', type=argparse.FileType('r'), help='sequence count table')
-    p.add_argument('--abund', '-a', type=float, default=10.0, help='minimum fold difference for comparing two OTUs (0=no abundance criterion; default 10.0)')
-    p.add_argument('--dist', '-d', type=float, default=0.1, help='maximum genetic difference for comparing two OTUs (default: 0.1)')
+    p.add_argument('fasta', type=argparse.FileType('r'), help='sequences (unaligned)')
+    p.add_argument('--kmer_length', '-k', type=int, default=4, help='kmer size')
+    p.add_argument('--abund', '-a', type=float, default=10, help='minimum fold difference for comparing two OTUs (0=no abundance criterion; default 10.0)')
+    p.add_argument('--dist', '-d', type=float, default=250.0, help='maximum genetic difference for comparing two OTUs (default: 0.1)')
     p.add_argument('--pval', '-p', type=float, default=0.0005, help='minimum p-value for merging OTUs (default: 0.0005)')
     p.add_argument('--log', '-l', default=None, type=argparse.FileType('w'), help='log output')
     p.add_argument('--verbose', '-v', action='store_true', help='record checks in log?')
@@ -204,11 +198,8 @@ if __name__ == '__main__':
     # read in the sequences table
     seq_table = read_sequence_table(args.table)
 
-    # read in the distance matrix
-    matrix = read_fasttree_matrix(args.matrix)
-
     # generate the caller object
-    caller = DBCaller(seq_table, matrix, args.dist, args.abund, args.pval, args.log, args.verbose)
+    caller = DBCaller(seq_table, SeqIO.parse(args.fasta, 'fasta'), args.kmer_length, args.dist, args.abund, args.pval, args.log, args.verbose)
     caller.generate_otu_table()
 
     caller.otu_table.to_csv(args.output, sep='\t')
