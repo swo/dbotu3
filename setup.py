b0a51d9191c9e7a55e19106c35f2ff72aeb8981b
swo <swo@mit.edu>
2016-10-30 17:12:02 -0400

    updated setup & reqs

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import find_packages, setup
+import pypandoc
 
 with open('LICENSE') as f:
     license=f.read()
 
 setup(name='dbotu',
-        version='0.1',
+        version='1.0',
         description='Distribution-based OTU calling',
+        long_description=pypandoc.convert('README.md', 'rst'),
         author='Scott Olesen',
-        author_email='swo@mit.edu',
+        author_email='swo@alum.mit.edu',
         platforms=['any'],
         license=license,
         url='http://github.com/swo/dbotu3',
+        data_files=[('input', ['data/input/counts.txt', 'data/input/seq.fa']),
+            ('output', ['data/output/log.txt', 'data/output/membership.txt', 'data/output/otu.txt'])],
         packages=find_packages(),
         )
