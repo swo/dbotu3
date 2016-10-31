4091e2055d2ca907b10c591bae5b375908d0b020
swo <swo@mit.edu>
2016-10-31 16:16:33 -0400

    improved setup doc

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ with open('LICENSE') as f:
     license=f.read()
 
 setup(name='dbotu',
-        version='1.0',
-        description='Distribution-based OTU calling',
-        long_description=pypandoc.convert('README.md', 'rst'),
-        author='Scott Olesen',
-        author_email='swo@alum.mit.edu',
-        platforms=['any'],
-        license=license,
-        url='http://github.com/swo/dbotu3',
-        data_files=[('input', ['data/input/counts.txt', 'data/input/seq.fa']),
-            ('output', ['data/output/log.txt', 'data/output/membership.txt', 'data/output/otu.txt'])],
-        packages=find_packages(),
-        )
+    version='1.0',
+    description='Distribution-based OTU calling',
+    long_description=pypandoc.convert('README.md', 'rst'),
+    author='Scott Olesen',
+    author_email='swo@alum.mit.edu',
+    platforms=['any'],
+    license=license,
+    url='http://github.com/swo/dbotu3',
+    data_files=[('input', ['data/input/counts.txt', 'data/input/seq.fa']),
+        ('output', ['data/output/log.txt', 'data/output/membership.txt', 'data/output/otu.txt'])],
+    classifiers=['Development Status :: 3', 'Environment :: Console',
+        'Programming Language :: Python :: 3', 'Topic :: Scientific/Engineering :: Bio-Informatics'],
+    packages=find_packages()
+    )
