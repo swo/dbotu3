9708c286a228c6001104104304108b8ce5b1a6d2
Claire Duvallet <cduvallet@gmail.com>
2016-11-28 03:09:52 +0000

    test_table files to StringIO

--- test/test_table.py
+++ test/test_table.py
@@ -2,13 +2,17 @@ import pytest
 from dbotu import read_sequence_table
 from Bio import SeqIO
 import os
+try:
+    from StringIO import StringIO
+except:
+    from io import StringIO
 
-tablefn = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'table_test.counts')
-fastafn = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'table_test.fasta')
+
 
 @pytest.fixture
 def read_table():
-    return read_sequence_table(tablefn)
+    table_fh = StringIO('\n'.join(['\tS1\tS2\tS3', '1\t0\t1\t5', '2\t10\t25\t5', '3\t4\t0\t0', '4\t0\t0\t1']))
+    return read_sequence_table(table_fh)
 
 def test_shape(read_table):
     assert read_table.shape == (4,3)
@@ -18,8 +22,9 @@ def test_index(read_table):
 
 def test_seqIDs(read_table):
     table = read_table
-    records = SeqIO.index(fastafn, 'fasta')
+    fasta_fh = StringIO('\n'.join(['>1', 'ATCG', '>2', 'TCGA', '>3', 'AGCT', '>4', 'TTCC']))
+    fasta_fh.seek(0)
+    records = SeqIO.to_dict(SeqIO.parse(fasta_fh, 'fasta'))
     missing_ids = [seq_id for seq_id in table.index if seq_id not in records]
     assert len(missing_ids) == 0
-
 
